a17033b0e5ec5ef3bf772111aab2159f2eeaaae1
Fuexfollets <fuex9follets@gmail.com>
2025-12-10 15:16:50 -0500

    Added database actions

--- image_enhance/session.py
+++ image_enhance/session.py
@@ -1,3 +1,5 @@
+import copy
+import fnmatch
 import pathlib
 import pickle
 import random
@@ -10,10 +12,12 @@ import image_enhance.model as imodel
 
 class Session:
     models: dict[str, imodel.EnhanceModel]
+    model_training_histories: dict[str, imodel.ModelTrainingHistory]
     databases: dict[str, list[idb.ImageSample]]
 
     def __init__(self):
         self.models = {}
+        self.model_training_histories = {}
         self.databases = {}
 
     def __getstate__(self):
@@ -47,12 +51,35 @@ class Session:
             return pickle.load(session_file)
 
     def import_glob(self, database_name: str, glob_pattern: str = "*"):
-        self.databases[database_name] = idb.import_glob(glob_pattern)
+        if database_name in self.databases:
+            self.databases[database_name].extend(idb.import_glob(glob_pattern))
+
+        else:
+            self.databases[database_name] = idb.import_glob(glob_pattern)
+
+    def corrupt_images(
+        self, database_name: str, image_name_glob: str = "*", iterations: int = 1
+    ):
+        for index in range(len(self.databases[database_name])):
+            if fnmatch.fnmatch(
+                self.databases[database_name][index].image_path.name, image_name_glob
+            ):
+                self.databases[database_name][index] = self.databases[database_name][
+                    index
+                ].corrupt(iterations)
 
     def merge_databases(self, merged_database_name: str, databases: list[str]):
         self.databases[merged_database_name] = sum(
             (self.databases[database_name] for database_name in databases), []
         )
+
+    def copy_database(self, source_database_name: str, target_database_name: str):
+        self.databases[target_database_name] = copy.deepcopy(
+            self.databases[source_database_name]
+        )
+
+    def delete_database(self, database_name: str):
+        del self.databases[database_name]
 
     def split_database(
         self,
@@ -72,6 +99,7 @@ class Session:
 
     def create_model(self, model_name: str):
         self.models[model_name] = imodel.EnhanceModel()
+        self.model_training_histories[model_name] = imodel.ModelTrainingHistory([])
 
     def get_training_samples(
         self,
@@ -131,3 +159,38 @@ class Session:
                     )
 
         return training_samples
+
+    def train_model(
+        self,
+        model_name: str,
+        database_name: str,
+        epochs: int = 1,
+        batch_size: int = 32,
+        learning_rate: float = 1e-3,
+        sample_expansion: int = 10,
+        display_progress: bool = False,
+        maximum_dimension: int = 256,
+        split_resize_ratio: float = 0.5,
+    ):
+        training_samples: list[imodel.ModelTrainingSample] = self.get_training_samples(
+            database_name,
+            sample_expansion=sample_expansion,
+            display_progress=display_progress,
+            maximum_dimension=maximum_dimension,
+            split_resize_ratio=split_resize_ratio,
+        )
+
+        random.shuffle(training_samples)
+
+        if display_progress:
+            print(f"Training model with {len(training_samples)} samples...")
+
+        self.model_training_histories[model_name] = imodel.train_model(
+            self.models[model_name],
+            training_samples,
+            epochs=epochs,
+            batch_size=batch_size,
+            existing_model_training_history=self.model_training_histories[model_name],
+            learning_rate=learning_rate,
+            display_progress=display_progress,
+        )
